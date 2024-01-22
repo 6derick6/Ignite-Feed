e4705ee96a57052c8b17bff11449fbe2e6ac0cab
Dérick <6derick6@gmail.com>
2024-01-22 19:43:20 -0300

    Propriedades

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,14 @@ import { Post } from './Post'
 export function App() {
   return (
     <div>
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+      <Post 
+        author="Diego Fernandes" 
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque aperiam a reprehenderit, quaerat aut, fugiat, eveniet ipsum culpa animi dicta unde dolor perspiciatis! Aliquid doloremque architecto, quae sequi alias inventore!" 
+      />
+      <Post 
+        author="Dérick Trennepohl" 
+        content="Um novo post muito legal"
+      />
     </div>
   )
 }
