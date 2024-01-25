f3cc1c24d5d521b715d2c485d256748e88323c89
Dérick <6derick6@gmail.com>
2024-01-24 21:52:24 -0300

    Finalizando Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import styles from './Sidebar.module.css';
+import { PencilLine } from 'phosphor-react';
 
 export function Sidebar() {
     return (
@@ -8,11 +9,15 @@ export function Sidebar() {
                 src="https://images.unsplash.com/photo-1587620962725-abab7fe55159?q=40&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
             />
             <div className={styles.profile}>
+                <img className={styles.avatar} src="https://github.com/6derick6.png" />
                 <strong>Dérick Trennepohl</strong>
                 <span>Web Developer</span>
             </div>
             <footer>
-                <a href="#">Editar seu perfil</a>
+                <a href="#">
+                    <PencilLine size={20} />
+                    Editar seu perfil
+                </a>
             </footer>
         </aside>
     ); 
