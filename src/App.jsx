d119580697343e0ae8946e45eee268f09f1a9ffa
Dérick <6derick6@gmail.com>
2024-01-24 21:26:49 -0300

    Componente: Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,27 @@
-import { Header } from './components/header'
-import { Post } from './Post'
-import './global.css'
+import { Header } from './components/header';
+import { Post } from './Post';
+import './global.css';
+import styles from './App.module.css';
+import { Sidebar } from './components/Sidebar';
 
 
 export function App() {
   return (
     <div>
       <Header />
-      <Post 
-        author="Diego Fernandes" 
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque aperiam a reprehenderit, quaerat aut, fugiat, eveniet ipsum culpa animi dicta unde dolor perspiciatis! Aliquid doloremque architecto, quae sequi alias inventore!" 
-      />
-      <Post 
-        author="Dérick Trennepohl" 
-        content="Um novo post muito legal"
-      />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post 
+          author="Diego Fernandes" 
+          content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque aperiam a reprehenderit, quaerat aut, fugiat, eveniet ipsum culpa animi dicta unde dolor perspiciatis! Aliquid doloremque architecto, quae sequi alias inventore!" 
+          />
+          <Post 
+            author="Dérick Trennepohl" 
+            content="Um novo post muito legal"
+          />
+        </main>
+      </div>
     </div>
   )
 }
