57a3e969c236d7a60ac1409330e205fa5b2beeb0
Dérick <6derick6@gmail.com>
2024-01-25 22:01:52 -0300

    Componente: Comment

---
+++ src/components/Comment.jsx
@@ -0,0 +1,34 @@
+import { Trash, ThumbsUp } from 'phosphor-react';
+import styles from './Comment.module.css';
+
+export function Comment() {
+    return (
+        <div className={styles.comment}>
+            <img src="https://github.com/6derick6.png" />
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Dérick Trennepohl</strong>
+                            <time title='24 de Janeiro às 22:18h' dateTime='2024-01-24 22:18:39'>Cerca de 1hr atrás</time>
+                        </div>
+
+                        <button title='Deletar comentário'>
+                            <Trash size={20} />
+                        </button>
+                    </header>
+
+                    <p>Muito bom Devon, parabéns!! 👏👏</p>
+                </div>
+
+                <footer>
+                    <button>
+                        <ThumbsUp />
+                        Aplaudir <span>20</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    )
+}
