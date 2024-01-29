06f600f6240061cd59f32d89c7863d55fbac7100
Dérick <6derick6@gmail.com>
2024-01-29 19:05:19 -0300

    Componente: Avatar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,6 @@
 import styles from './Sidebar.module.css';
 import { PencilLine } from 'phosphor-react';
+import { Avatar } from './Avatar';
 
 export function Sidebar() {
     return (
@@ -9,7 +10,7 @@ export function Sidebar() {
                 src="https://images.unsplash.com/photo-1587620962725-abab7fe55159?q=40&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
             />
             <div className={styles.profile}>
-                <img className={styles.avatar} src="https://github.com/6derick6.png" />
+                <Avatar src="https://github.com/6derick6.png" />
                 <strong>Dérick Trennepohl</strong>
                 <span>Web Developer</span>
             </div>
